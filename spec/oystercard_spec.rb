6c202007670e86f874b8bc325fec30cb50237450
Leo Brown <leo_brown_1@hotmail.com>
2021-09-28 14:29:46 +0100

    Step 5 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe OysterCard do
   it "starts with a default balance of 0" do
     expect(subject.balance).to eq 0
   end
+
+  it "tops up the card with money" do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
 end
+
+
